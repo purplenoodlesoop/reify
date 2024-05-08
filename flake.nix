54eadf9d821948931fba211b363832365e58716e
Yakov Karpov <yakovkarpoff@gmail.com>
2024-05-08 16:42:21 +0300

    Update watch develop/app

--- flake.nix
+++ flake.nix
@@ -21,5 +21,23 @@
     in
     core.mkFlake {
       inherit name systemSpecific;
+      lib.build.watch = { pkgs, bin }: with pkgs; writeShellApplication {
+        name = "watch";
+        runtimeInputs = [
+          dart
+          nodePackages_latest.live-server
+        ];
+        text = ''
+          (trap 'kill 0' SIGINT; dart \
+            --enable-vm-service \
+            ${bin}.dart \
+            --root=./site \
+            --watch \
+            --mode=local \
+            --path=./site/input \
+            --path=./lib & \
+          live-server ./site/output & wait)
+        '';
+      };
     };
 }
