d4e17bfe92e5fde0846566e306bd95d1f6a708cb
Yakov Karpov <yakovkarpoff@gmail.com>
2024-06-16 04:14:00 +0300

    Add exports

--- packages/web_reify/lib/web_reify.dart
+++ packages/web_reify/lib/web_reify.dart
@@ -1,3 +1,13 @@
 library web_reify;
 
 export 'package:markdown/markdown.dart' show Element, Node, Text;
+
+export 'src/base_page.dart';
+export 'src/copy_static.dart';
+export 'src/create_site.dart';
+export 'src/document_contents.dart';
+export 'src/html.dart';
+export 'src/markdown.dart';
+export 'src/robots.dart';
+export 'src/rss.dart';
+export 'src/sitemap.dart';
